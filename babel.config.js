08f3e146ac703424c1d744e546bcd239b75ff6f8
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-16 19:54:05 -0300

    chore: Added path mapping and initial structure

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,29 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+       [
+        "module-resolver",
+        {
+          "root": ["./src"],
+          "alias": {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@routes": "./src/routes",
+            "@hooks": "./src/hooks",
+            "@context": "./src/context",
+            "@services": "./src/services",
+            "@config": "./src/config",
+            "@constants": "./src/constants",
+            "@styles": "./src/styles",
+            "@types": "./src/types",
+            "@theme": "./src/theme",
+            "@storage": "./src/storage",
+          }
+       }
+      ]
+    ]
   };
 };
