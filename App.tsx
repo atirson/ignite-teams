411fe422d44b2c5006a97ef3e2cee2ecffe6ac45
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-18 21:40:15 -0300

    config: Added configuration to use Styled Components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import { Groups } from '@screens/Groups';
 
+import theme from './src/theme'
+
+
 export default function App() {
+  const [ fontsLoader ] = useFonts({  
+      Roboto_400Regular,
+      Roboto_700Bold
+  });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      { fontsLoader ? <Groups /> : <ActivityIndicator size="large" color="#000" />}
+    </ThemeProvider>
   );
 }
