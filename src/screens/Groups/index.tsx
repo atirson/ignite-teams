1e332937d9e976a9208b62fe620951c0e7fb8023
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-20 20:01:21 -0300

    feat: Added custom StatusBar

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
 
 export function Groups() {
   return (
-    <View>
+    <Container>
       <Text>Groups</Text>
-    </View>
+    </Container>
   );
 }
 
