5ef510285ba463b85108f4ae55442ff64d8b53e1
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-23 19:02:41 -0300

    feat: Added Flatlist component to list all groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocketseat', 'Turma Ignite', 'Turma React Native']);
+
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title='Turma Ignite' />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+      />
+
     </Container>
   );
 }
