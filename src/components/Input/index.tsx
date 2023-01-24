c82d1b3ad75e600d52d764f2083980a11f0f4411
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-24 20:57:48 -0300

    feat: Added component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import { TextInputProps } from 'react-native';
+import { Container } from './style';
+
+import { useTheme } from 'styled-components/native'
+
+export const Input = ({...rest}: TextInputProps) => {
+  const { COLORS } = useTheme()
+
+  return (
+    <Container placeholderTextColor={COLORS.GRAY_300} {...rest}/>
+  )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { Input } from '@components/Input'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
@@ -12,10 +13,10 @@ export function NewGroup() {
         <Icon />
         <Highlight 
           title="Nova turma"
-          subtitle='crie a turma para adicionar as pessoas'
+          subtitle="crie a turma para adicionar as pessoas"
         />
-          
-        <Button title='Criar' />
+        <Input placeholder="Nome da turma" />  
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   )
