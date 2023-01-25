322777253b328b1a068a8818ab9230e1f06faff0
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-01-25 20:37:25 -0300

    feat: Added function goBackHome

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,9 +2,16 @@ import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { useNavigation } from '@react-navigation/native'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+  const navigation = useNavigation()
+
+  const handleNew = () => {
+    navigation.navigate('players', {  group: 'Turma React Native'})
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +23,11 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
         <Input placeholder="Nome da turma" />  
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button 
+          title="Criar" 
+          style={{ marginTop: 20 }} 
+          onPress={handleNew}
+        />
       </Content>
     </Container>
   )
